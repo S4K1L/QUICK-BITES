7fccbf15c6bfaecab49db24b49881b9f5a74b872
s4k1l <69shakilmahmud@gmail.com>
2024-06-10 15:00:42 +0600

    version: 10.06.2024 v3

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'Presentation/User_Panel/User_HomePage/menu_Screen.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
