e23b1e0178212439f65db100a5a329a5a76bb010
Shakil Mahmud <69shakilmahmud@gmail.com>
2024-07-07 12:15:11 +0600

    version: 07.07.2024(Final Version)

--- lib/Presentation/Drawer/admin_Drawer.dart
+++ lib/Presentation/Drawer/admin_Drawer.dart
@@ -1,19 +1,51 @@
 // ignore_for_file: file_names, use_build_context_synchronously
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:quick_bites/Presentation/Admin_Panel/Approve/account_control.dart';
 import 'package:quick_bites/Presentation/Admin_Panel/Finance/total_finance.dart';
 import 'package:quick_bites/Presentation/Chef_Panel/Chef_login/chef_login.dart';
+import 'package:quick_bites/Presentation/Welcome_Screen/welcome_screen.dart';
 import '../../Core/Repository_and_Authentication/profile_image_picker.dart';
 import '../../Theme/const.dart';
 import '../../Theme/constant.dart';
 import '../Admin_Panel/Admin_HomePage/Admin_Home_Screen.dart';
 import '../Admin_Panel/List/chef_list.dart';
 import '../Admin_Panel/List/runner_list.dart';
+import 'edit_profile.dart';
 
 
-class AdminDrawer extends StatelessWidget {
+class AdminDrawer extends StatefulWidget {
   const AdminDrawer({super.key});
+
+  @override
+  State<AdminDrawer> createState() => _AdminDrawerState();
+}
+
+class _AdminDrawerState extends State<AdminDrawer> {
+  String _name = '';
+  String _email = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchUserData();
+  }
+
+  Future<void> _fetchUserData() async {
+    User? firebaseUser = FirebaseAuth.instance.currentUser;
+    if (firebaseUser != null) {
+      DocumentSnapshot userDoc = await FirebaseFirestore.instance.collection('users').doc(firebaseUser.uid).get();
+      setState(() {
+        _name = userDoc['name'] ?? '';
+        _email = userDoc['email'] ?? '';
+      });
+    }
+  }
+
+  void _signOut() async {
+    await FirebaseAuth.instance.signOut();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,23 +69,35 @@ class AdminDrawer extends StatelessWidget {
               ),
               child: Column(
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 30),
-                    child: Container(
-                      width: 145,
-                      height: 145,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(70),
-                        color: kTextWhiteColor,
-                      ),
-                      child: const Center(child: ProfileImagePicker()),
+                  Align(
+                      alignment: Alignment.centerRight,
+                      child: IconButton(onPressed: (){
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => EditUserProfile(name: _name, email: _email),
+                          ),
+                        );
+                      }, icon: const Icon(Icons.edit,color: Colors.lightGreen,))),
+                  Container(
+                    width: 90,
+                    height: 90,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(70),
+                      color: kTextWhiteColor,
                     ),
+                    child: const Center(child: ProfileImagePicker()),
                   ),
                   const SizedBox(height: 20),
-                  const Text(
-                    'Admin Profile',
-                    style: TextStyle(fontSize: 20, color: sBlackColor),
-                  )
+                  Text(
+                    style: const TextStyle(fontSize: 20, color: sBlackColor),
+                    _name,
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    style: const TextStyle(fontSize: 16, color: sBlackColor),
+                    'Email : $_email',
+                  ),
                 ],
               ),
             ),
@@ -97,7 +141,7 @@ class AdminDrawer extends StatelessWidget {
                     );
                   },
                 ),
-        
+
                 const SizedBox(height: 20),
                 _buildDrawerButton(
                   context,
@@ -110,7 +154,7 @@ class AdminDrawer extends StatelessWidget {
                     );
                   },
                 ),
-        
+
                 const SizedBox(height: 20),
                 _buildDrawerButton(
                   context,
@@ -128,12 +172,12 @@ class AdminDrawer extends StatelessWidget {
                   context,
                   icon: Icons.logout,
                   label: 'Logout',
-                  onPressed: () async {
-                    await FirebaseAuth.instance.signOut();
+                  onPressed: (){
+                    _signOut();
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => const ChefLoginScreen(),
+                        builder: (context) => const WelcomeScreen(),
                       ),
                     );
                   },
